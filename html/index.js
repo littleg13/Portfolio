0bc62511c88a2dbc94db2b3033963cfdf46a278d
Grady Wright <gowright98@gmail.com>
2020-11-03 14:33:09 -0600

    Added functionality to skill cards and appeased Taylor

--- html/index.js
+++ html/index.js
@@ -1,9 +1,21 @@
 let skills = [];
+let indicators = [];
 let currentActiveSkill = 1;
 let skillsCarousel;
+let skillIndicator;
 window.onload = (event) => {
     skillsCarousel = document.getElementById("skillsCarousel");
+    skillIndicator = document.getElementById("skillIndicator");
     skills = Array.from(document.getElementsByClassName("skill"));
+    for(let i=0;i<skills.length;i++){
+        let node = document.createElement("i");
+        node.className = "fas fa-circle";
+        indicators.push(node);
+        skillIndicator.prepend(node);
+    }
+    indicators[1].classList.add("indicator-active");
+    document.getElementsByClassName("skill-left")[0].addEventListener("click", advSkillRight);
+    document.getElementsByClassName("skill-right")[0].addEventListener("click", advSkillLeft);
     let projects = document.getElementsByClassName("project");
     for(let i=0;i<projects.length;i++){
         projects[i].addEventListener("click", expandProject);
@@ -17,24 +29,36 @@ window.onload = (event) => {
 
 function advSkillRight() {
     skills[getRightIndex()].className = "skill skill-inactive";
+    skills[getRightIndex()].removeEventListener("click", advSkillLeft);
 
     skills[currentActiveSkill].className = "skill skill-right";
+    skills[currentActiveSkill].addEventListener("click", advSkillLeft);
+    indicators[currentActiveSkill].classList.remove("indicator-active");
 
     currentActiveSkill = getLeftIndex();
     skills[currentActiveSkill].className = "skill skill-active";
+    skills[currentActiveSkill].removeEventListener("click", advSkillRight);
+    indicators[currentActiveSkill].classList.add("indicator-active");
 
     skills[getLeftIndex()].className = "skill skill-left";
+    skills[getLeftIndex()].addEventListener("click", advSkillRight);
 }
 
 function advSkillLeft() {
     skills[getLeftIndex()].className = "skill skill-inactive";
+    skills[getLeftIndex()].removeEventListener("click", advSkillRight);
 
     skills[currentActiveSkill].className = "skill skill-left";
+    skills[currentActiveSkill].addEventListener("click", advSkillRight);
+    indicators[currentActiveSkill].classList.remove("indicator-active");
 
     currentActiveSkill = getRightIndex();
     skills[currentActiveSkill].className = "skill skill-active";
+    skills[currentActiveSkill].removeEventListener("click", advSkillLeft);
+    indicators[currentActiveSkill].classList.add("indicator-active");
 
     skills[getRightIndex()].className = "skill skill-right";
+    skills[getRightIndex()].addEventListener("click", advSkillLeft);
 }
 
 function getLeftIndex() {
